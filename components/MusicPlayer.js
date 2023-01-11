fd72f70e42a5d4d4516ca656fb8693297a4c12d3
Wilson Voltaire <wilsonvoltaire@gmail.com>
2023-01-10 20:25:31 -0500

    sliding bar works, but need to fix song update

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -5,24 +5,64 @@ import YoutubePlayer from 'youtube-player';
 import { useEffect, useState } from "react";
 
 
-export default function MusicPlayer() {
-  let player;
+const MusicPlayer = () => {
+  let duration_update;
 
-  const [localPlayerState, setLocalPlayerState] = useState(-1);
-  console.log("Local State", localPlayerState)
+  const [songTime, setSongTime] = useState({ seconds: 0, text: '00:00:00' });
+  const [player, setPlayer] = useState(null)
+  const [barValue, setBarValue] = useState(0)
+
 
   const currentSong = {
     title: `God's Plan`,
     thumbnail: '../assets/scorpion_cover.jpg',
     videoId: 'm1a_GqJf02M',
-    duration: 0,
+    duration: 1,
+    //duration_text: '00:00:00'
   }
+
+  useEffect(async () => {
+    console.log(songTime)
+    try {
+      setBarValue((songTime.seconds / await player.getDuration()) * 100)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [songTime])
 
   useEffect(() => {
     addMusicPlayer();
     console.log("Song Duration:", currentSong.duration)
+  }, [])
 
-  })
+  useEffect(async function () {
+    console.log("Player Value Changed\n", "Player is now", player)
+    try {
+
+      player.addEventListener('onStateChange', async (event) => {
+        const newState = await player.getPlayerState()
+        if (newState === 1) {
+          startDurationUpdate()
+          console.log("STARTED DURATION UPDATE")
+        } else {
+          stopDurationUpdate()
+          console.log("STOPPED DURATION UPDATE")
+        }
+      })
+
+      // await player.loadVideoById(currentSong.videoId)
+      await player.unMute()
+      await player.setPlaybackQuality('small')
+      currentSong.duration = await player.getDuration()
+      currentSong.duration_text = new Date(currentSong.duration * 1000).toISOString().substring(11, 19);
+      console.log("Duration is", currentSong.duration_text)
+      // await player.playVideo()
+    } catch (error) {
+      console.log(error)
+    }
+
+
+  }, [player])
 
   async function addMusicPlayer() {
     console.log("Going to play music...", this)
@@ -31,7 +71,7 @@ export default function MusicPlayer() {
       player_div.setAttribute('id', 'video-player')
       document.body.appendChild(player_div)
 
-      player = YoutubePlayer('video-player', {
+      setPlayer(YoutubePlayer('video-player', {
         // height: '0',
         // width: '0',
         videoId: currentSong.videoId,
@@ -40,55 +80,102 @@ export default function MusicPlayer() {
           loop: false,
         },
 
-      })
-      player.loadVideoById(currentSong.videoId)
-      player.unMute()
+      }))
 
-      await player.playVideo()
-      //setLocalPlayerState(await player.getPlayerState())
-      //currentSong.duration = await player.getDuration()
-      //console.log("Duration Found", currentSong.duration)
-      //console.log("Playing video...", player)
+      // setTimeout(async () => {
+      //   currentSong.duration = await player.getDuration()
+      //   console.log("Duration Found", currentSong.duration)
+      // }, 1000);
+
     } catch (error) {
       console.log(error)
     }
   }
 
-  const playPause = async function (event) {
+  const playPause = async function () {
     const player_state = await player.getPlayerState()
-    const button = event.target
+    const pause_button = document.getElementById('pause-button')
+    const play_button = document.getElementById('play-button')
 
-    console.log(button)
-
-    if (button.id === 'play-button') {
-      button.hidden = 'hidden'
-      const pause_button = document.getElementById('pause-button')
-      pause_button.hidden = '';
-
-    } else if (button.id === 'pause-button') {
-      button.hidden = 'hidden'
-
-      const play_button = document.getElementById('play-button')
-      play_button.hidden = '';
-    }
-
-    if (player_state === 2) {
+    if ([-1, 2].includes(player_state)) {
       await player.playVideo()
+      play_button.style.display = 'none';
+      pause_button.style.display = 'block';
+
       console.log("Played Video")
-      event.target.name = 'play-sharp'
+      // currentSong.duration = await player.getDuration()
+      // console.log("Duration Found", currentSong.duration)
+
     } else if (player_state === 1) {
       await player.pauseVideo()
+      play_button.style.display = 'block';
+      pause_button.style.display = 'none';
+
       console.log("Paused Video")
-      event.target.name = 'pause-sharp'
+
+    }
+  }
+
+
+  const startDurationUpdate = async function () {
+    try {
+      //Update from video when starting
+      const fetchedSeconds = await player.getCurrentTime();
+
+      setSongTime({
+        seconds: fetchedSeconds,
+        ...songTime
+      })
+
+      //await new Promise(resolve => setTimeout(resolve, 3000))
+
+      duration_update = setInterval(() => {
+        updateSongDuration()
+      }, 1000);
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  const stopDurationUpdate = function () {
+    clearInterval(duration_update);
+    duration_update = null;
+  }
+
+  const updateSongDuration = async function () {
+    try {
+      let { seconds, text } = songTime;
+      seconds = songTime.seconds++
+
+      text = new Date(seconds * 1000).toISOString().substring(11, 19)
+      console.log("Text is supposed to be", text)
+
+      // console.log("New Info:", {
+      //   seconds,
+      //   text,
+      // })
+
+      // const prog_bar = document.getElementById('song-progress')
+      // console.log(prog_bar)
+      // let bar_value = (seconds / await player.getDuration()) * 100;
+      // console.log("bar value", bar_value)
+      // prog_bar.style.flexGrow = bar_value
+      setSongTime({
+        seconds,
+        text
+      })
+
+      // console.log("SongTime is now", songTime)
+    } catch (error) {
+      console.log(error)
     }
   }
 
   return (
     <ImageBackground style={styles.backgroundImage} source={require('../assets/scorpion_cover.jpg')}>
-      {/* <View nativeID='video-player'><Text>This is the music player{console.log("Player DIV created")}</Text></View> */}
-      {/* {addMusicPlayer()} */}
       <View style={styles.container}>
-
 
         <View style={styles.songTitleContainer}>
           <Text style={styles.songTitle}>{currentSong.title}</Text>
@@ -99,21 +186,26 @@ export default function MusicPlayer() {
         </View>
 
         <View style={styles.playSlider}>
-          <Text>0:00 | {localPlayerState}</Text>
-          <Slider minimumValue={0} maximumValue={100} value={0} />
+          <Text nativeID="song-duration">{songTime.text}</Text>
+          <Slider minimumValue={0} maximumValue={100} value={barValue} nativeID="song-progress" onSlidingComplete={async (newValue) => {
+            let newNumber = (newValue / 100) * await player.getDuration();
+            console.log("Seeking to ", newNumber)
+            await player.seekTo(newNumber)
+            setSongTime({ ...songTime, seconds: newNumber })
+          }} />
         </View>
 
         <View style={styles.playButtons}>
           <Ionicons name='shuffle-sharp' size={24} />
           <Ionicons name='play-back-sharp' size={24} />
           <Ionicons name='play-sharp' nativeID="play-button" size={24} onPress={playPause} />
-          <Ionicons name='pause-sharp' nativeID="pause-button" size={24} onPress={playPause} />
+          <Ionicons name='pause-sharp' nativeID="pause-button" size={24} style={styles.hidden} onPress={playPause} />
           <Ionicons name='play-forward-sharp' size={24} />
           <Ionicons name='repeat-sharp' size={24} />
         </View>
 
       </View>
-    </ImageBackground>
+    </ImageBackground >
   );
 }
 
@@ -158,5 +250,11 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-evenly'
+  },
+  hidden: {
+    display: 'none'
   }
 })
+
+
+export default MusicPlayer;
